2e7972cafe0d477eed8de80e9ee2185fc887d752
Arivindraj <35523309+Arivindraj@users.noreply.github.com>
2022-07-10 10:31:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,18 @@ st.set_page_config(
          'About': "This is an *extremely* cool app powered by Snowpark for Python, Streamlit, and Snowflake Data Marketplace"
      }
  )
+
+ 
+streamlit.header("View Our Fruit List - Add Your Favorites!")
+# Snowflake related function
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select top 100 * from ENVIRONMENT_DATA_ATLAS ")
+    return my_cur.fetchall()
+  
+# Add a button to load the fruit
+if streamlit.button("Get Fruit List"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
