9636496e6f2fb93982d3241e961a5456b81cb370
Arivindraj <35523309+Arivindraj@users.noreply.github.com>
2022-07-10 11:29:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,12 +26,28 @@ def create_session_object():
     my_cur.execute("select current_warehouse(), current_database(), current_schema()")
     return my_cur.fetchall()
 
-# Add a button to load teh fruit
+# Add a button to load the Connection Details
 if st.button("Get Connection Details"):
   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   my_data_rows = create_session_object()
   my_cnx.close()
   st.dataframe(my_data_rows)
+
+def get_the_data():
+  with my_cnx.cursor() as my_cur:
+    #my_cur.execute("select current_warehouse(), current_database(), current_schema()")
+    snow_df_co2 = my_cur.table("ENVIRONMENT.EDGARED2019").filter(col('Indicator Name') == 'Fossil CO2 Emissions').filter(col('Type Name') == 'All Type')
+    snow_df_co2 = snow_df_co2.group_by('Location Name').agg(sum('$16').alias("Total CO2 Emissions")).filter(col('Location Name') != 'World').sort('Location Name')
+    pd_df_co2  = snow_df_co2.to_pandas()
+    return pd_df_co2.fetchall()
+     
+ # Add a button to load the Connection Details
+if st.button("Get Data"):
+  my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+  my_data = get_the_data()
+  my_cnx.close()
+  st.dataframe(my_data)
+
 
 # Add header and a subheader
 st.header("Knoema: Environment Data Atlas")
