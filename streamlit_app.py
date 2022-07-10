09484038e3bed546451aad6582a286e157bb48b0
Arivindraj <35523309+Arivindraj@users.noreply.github.com>
2022-07-10 10:03:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,16 @@
-import streamlit
+import streamlit as st
+import pandas as pd
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
-streamlit.title("My Second App")
+st.set_page_config(
+     page_title="Environment Data Atlas",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://developers.snowflake.com',
+         'About': "This is an *extremely* cool app powered by Snowpark for Python, Streamlit, and Snowflake Data Marketplace"
+     }
+ )
